887c67edc35c48443225162bfeea9fc241cb4dcb
Patrick Lavin <plavin997@gmail.com>
2023-03-09 00:29:46 -0500

    initial version of parrot, not working yet

---
+++ src/sst/elements/memHierarchy/parrot.cc
@@ -0,0 +1,199 @@
+// Copyright 2013-2022 NTESS. Under the terms
+// of Contract DE-NA0003525 with NTESS, the U.S.
+// Government retains certain rights in this software.
+//
+// Copyright (c) 2013-2022, NTESS
+// All rights reserved.
+//
+// Portions are copyright of other developers:
+// See the file CONTRIBUTORS.TXT in the top level directory
+// of the distribution for more information.
+//
+// This file is part of the SST software package. For license
+// information, see the LICENSE file in the top level directory of the
+// distribution.
+
+#include <sst_config.h>
+#include "parrot.h"
+
+#include <sst/core/params.h>
+#include <sst/core/interfaces/stringEvent.h>
+
+using namespace SST;
+using namespace SST::MemHierarchy;
+using namespace std;
+
+
+Parrot::Parrot(ComponentId_t id, Params &params) : Component(id) {
+    /* Setup output and debug streams */
+    output.init("", 1, 0, Output::STDOUT);
+
+    int debugLevel = params.find<int>("debug_level", 0);
+    debug.init("", debugLevel, 0, (Output::output_location_t)params.find<int>("debug", 0));
+
+    std::vector<Addr> addrArr;
+    params.find_array<Addr>("debug_addr", addrArr);
+    for (std::vector<Addr>::iterator it = addrArr.begin(); it != addrArr.end(); it++)
+        DEBUG_ADDR.insert(*it);
+
+    /* Setup clock */
+    clockHandler = new Clock::Handler<Parrot>(this, &Parrot::tick);
+    clock = registerClock(params.find<std::string>("clock", "1GHz"), clockHandler);
+    clockOn = true;
+    timestamp = 0;
+
+
+    /* Setup up links */
+    if (isPortConnected("high_network_0")) {
+        SST::Link *link = configureLink("high_network_0", "50ps", new Event::Handler<Parrot>(this, &Parrot::handleResponse));
+        if (!link)
+            output.fatal(CALL_INFO, -1, "%s, Error: unable to configure link on port high_network_0.\n", getName().c_str());
+        upLinks.push_back(link);
+    } else {
+        output.fatal(CALL_INFO, -1, "%s, Error: no connected 'high_network_0' port.\n", getName().c_str());
+    }
+    int num_links = 1;
+    std::string linkname = "high_network_1";
+    while (isPortConnected(linkname)) {
+        SST::Link *link = configureLink(linkname, "50ps", new Event::Handler<Parrot>(this, &Parrot::handleResponse));
+        if (!link)
+            output.fatal(CALL_INFO, -1, "%s, Error: unable to configure link on port '%s'\n", getName().c_str(), linkname.c_str());
+        upLinks.push_back(link);
+        num_links++;
+        linkname = "high_network_" + std::to_string(num_links);
+    }
+
+    /* Setup down links */
+    if (isPortConnected("low_network_0")) {
+        SST::Link *link = configureLink("low_network_0", "50ps", new Event::Handler<Parrot>(this, &Parrot::handleResponse));
+        if (!link)
+            output.fatal(CALL_INFO, -1, "%s, Error: unable to configure link on port low_network_0.\n", getName().c_str());
+        downLinks.push_back(link);
+    } else {
+        output.fatal(CALL_INFO, -1, "%s, Error: no connected 'low_network_0' port.\n", getName().c_str());
+    }
+
+    for ( int i = 1; i < num_links; i++) {
+        std::string linkname = "low_network_" + std::to_string(i);
+        if (isPortConnected(linkname)) {
+            SST::Link *link = configureLink(linkname, "50ps", new Event::Handler<Parrot>(this, &Parrot::handleResponse));
+            if (!link)
+                output.fatal(CALL_INFO, -1, "%s, Error: unable to configure link on port '%s'\n", getName().c_str(), linkname.c_str());
+            downLinks.push_back(link);
+        } else {
+            output.fatal(CALL_INFO, -1, "%s, Error: Expected %d low_network links but port '%s' not connected.\n", getName().c_str(), num_links, linkname.c_str());
+        }
+    }
+
+
+
+    /* Setup throughput limiting */
+    requestsPerCycle = params.find<uint64_t>("requests_per_cycle", 0);
+    responsesPerCycle = params.find<uint64_t>("responses_per_cycle", 0);
+}
+
+Parrot::~Parrot() {
+    while (requestQueue.size()) {
+        delete requestQueue.front();
+        requestQueue.pop();
+    }
+    while (responseQueue.size()) {
+        delete responseQueue.front();
+        responseQueue.pop();
+    }
+}
+
+void Parrot::handleRequest(SST::Event * ev, unsigned int threadid) {
+    MemEventBase *event = static_cast<MemEventBase*>(ev);
+    if (!clockOn) enableClock();
+    //TODO: Optimize - give each link pair its own map
+    threadRequestMap.insert(std::make_pair(event->getID(), threadid));
+    requestQueue.push(event);
+}
+
+void Parrot::handleResponse(SST::Event * ev) {
+    MemEventBase *event = static_cast<MemEventBase*>(ev);
+    if (!clockOn) enableClock();
+    responseQueue.push(event);
+}
+
+bool Parrot::tick(SST::Cycle_t cycle) {
+    timestamp++;
+
+    uint64_t sendcount = (requestsPerCycle == 0) ? requestQueue.size() : requestsPerCycle;
+
+    /* Drain request queue */
+    while (!requestQueue.empty() && sendcount > 0) {
+        MemEventBase * event = requestQueue.front();
+        unsigned int linkid = threadRequestMap.find(event->getID())->second;
+        downLinks[linkid]->send(event);
+        requestQueue.pop();
+        sendcount--;
+    }
+
+    sendcount = (responsesPerCycle == 0) ? responseQueue.size() : responsesPerCycle;
+
+    /* Drain response queue */
+    while (!responseQueue.empty() && sendcount > 0) {
+        MemEventBase * event = responseQueue.front();
+        responseQueue.pop();
+
+        unsigned int linkid = threadRequestMap.find(event->getResponseToID())->second;
+        threadRequestMap.erase(event->getResponseToID());
+        upLinks[linkid]->send(event);
+
+        sendcount--;
+    }
+
+    /* Turn off clock if queues are empty */
+    if (requestQueue.empty() && responseQueue.empty()) {
+        clockOn = false;
+        return true;
+    }
+    return false;
+}
+
+inline void Parrot::enableClock() {
+    clockOn = true;
+    timestamp = reregisterClock(clock, clockHandler);
+    timestamp--;
+}
+
+/** SST init/finish */
+void Parrot::setup() {}
+
+void Parrot::finish() {}
+
+/*
+ *  Init:
+ *      forward all CPU events to memory hierarchy
+ *      Broadcast L1's 'SST::MemHierarchy::MemEvent' event to all CPUs
+ *
+ */
+void Parrot::init(unsigned int phase) {
+    SST::Event * ev;
+
+    // Pass CPU events to memory hierarchy, generally these are memory initialization
+    for (int i = 0; i < upLinks.size(); i++) {
+        while ((ev = upLinks[i]->recvInitData()) != NULL) {
+            MemEventInit * memEvent = dynamic_cast<MemEventInit*>(ev);
+            if (memEvent) {
+                downLinks[i]->sendInitData(memEvent->clone());
+            }
+            delete ev;
+        }
+    }
+
+    for (int i = 0; i < downLinks.size(); i++) {
+        while ((ev = downLinks[i]->recvInitData()) != NULL) {
+            MemEventInit * memEvent = dynamic_cast<MemEventInit*>(ev);
+            if (memEvent) {
+                upLinks[i]->sendInitData(memEvent->clone());
+            }
+            delete ev;
+        }
+    }
+    printf("PARROT: FINISHING INIT PHASE\n");
+
+}
+
---
+++ src/sst/elements/memHierarchy/parrot.h
@@ -0,0 +1,104 @@
+// Copyright 2013-2022 NTESS. Under the terms
+// of Contract DE-NA0003525 with NTESS, the U.S.
+// Government retains certain rights in this software.
+//
+// Copyright (c) 2013-2022, NTESS
+// All rights reserved.
+//
+// Portions are copyright of other developers:
+// See the file CONTRIBUTORS.TXT in the top level directory
+// of the distribution for more information.
+//
+// This file is part of the SST software package. For license
+// information, see the LICENSE file in the top level directory of the
+// distribution.
+
+#ifndef _MEMHIERARCHY_PARROT_
+#define _MEMHIERARCHY_PARROT_
+
+#include <map>
+#include <queue>
+
+#include <sst/core/event.h>
+#include <sst/core/sst_types.h>
+#include <sst/core/component.h>
+#include <sst/core/link.h>
+#include <sst/core/timeConverter.h>
+#include <sst/core/output.h>
+
+#include "sst/elements/memHierarchy/memEventBase.h"
+#include "sst/elements/memHierarchy/util.h"
+
+using namespace std;
+
+namespace SST { namespace MemHierarchy {
+
+class Parrot : public Component {
+public:
+/* Element Library Info */
+    SST_ELI_REGISTER_COMPONENT(Parrot, "memHierarchy", "Parrot", SST_ELI_ELEMENT_VERSION(1,0,0),
+            "Layer that learns the memory behavior per-phase and mimics it", COMPONENT_CATEGORY_MEMORY)
+
+    SST_ELI_DOCUMENT_PARAMS(
+            {"clock",               "(string) Clock frequency or period with units (Hz or s; SI units OK).", NULL},
+            {"requests_per_cycle",  "(uint) Number of requests to forward to L1 each cycle (for all threads combined). 0 indicates unlimited", "0"},
+            {"responses_per_cycle", "(uint) Number of responses to forward to threads each cycle (for all threads combined). 0 indicates unlimited", "0"},
+            {"debug",               "(uint) Where to print debug output. Options: 0[no output], 1[stdout], 2[stderr], 3[file]", "0"},
+            {"debug_level",         "(uint) Debug verbosity level. Between 0 and 10", "0"},
+            {"debug_addr",          "(comma separated uint) Address(es) to be debugged. Leave empty for all, otherwise specify one or more, comma-separated values. Start and end string with brackets",""} )
+
+    SST_ELI_DOCUMENT_PORTS(
+          {"low_network_%(port)d", "Link to lower levels", {"memHierarchy.MemEventBase"} },
+          {"high_network_%(port)d", "Links to higher level", {"memHierarchy.MemEventBase"} } )
+
+/* Begin class definition */
+    /** Constructor & destructor */
+    Parrot(ComponentId_t id, Params &params);
+    ~Parrot();
+
+    /** SST component basic functions */
+    void setup(void);
+    void init(unsigned int phase);
+    void finish(void);
+
+    /** Handles response from memory hierarchy, passes to correct thread's CPU */
+    void handleResponse(SST::Event *event);
+
+    /** Handles request from CPU, passes on to memory hierarchy */
+    void handleRequest(SST::Event *event, unsigned int threadid);
+
+    /** Clock handler - requests/responses are sent on cycle boundaries */
+    bool tick(SST::Cycle_t cycle);
+
+private:
+    /** Output and debug */
+    Output debug;
+    Output output;
+    std::set<Addr> DEBUG_ADDR;
+
+    /** Links */
+    vector<SST::Link*> upLinks;
+    vector<SST::Link*> downLinks;
+
+    /** Timestamp & clock control */
+    uint64_t    timestamp;
+    bool        clockOn;
+    Clock::Handler<Parrot>*  clockHandler;
+    TimeConverter* clock;
+
+    /** Track outstanding requests for routing responses correctly */
+    std::map<Event::id_type, unsigned int> threadRequestMap;
+
+    /** Throughput control */
+    uint64_t requestsPerCycle;
+    uint64_t responsesPerCycle;
+    std::queue<MemEventBase*> requestQueue;
+    std::queue<MemEventBase*> responseQueue;
+
+    inline void enableClock();
+};
+
+}
+}
+
+#endif /* _MEMHIERARCHY_PARROT_ */
