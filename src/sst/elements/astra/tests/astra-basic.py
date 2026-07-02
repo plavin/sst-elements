ae3774c269146ab9e7a089beb7f07f140282a1fe
Patrick Lavin <plavin997@gmail.com>
2026-07-02 12:01:24 -0600

    switch to new merlin config

--- src/sst/elements/astra/tests/astra-basic.py
+++ src/sst/elements/astra/tests/astra-basic.py
@@ -3,21 +3,35 @@
 #  the Merlin config, and possibly in other astra input files.
 
 import sst
+
 import os
 import json
 import pathlib
-from sst.merlin import *
 
-astra_dir    = pathlib.Path('/home/prlavin/sst/sst-astra-2/astra-sim') # TODO - set from ASTRA_ROOT or smth
+from sst.merlin.base import *
+from sst.merlin.endpoint import *
+from sst.merlin.topology import *
+from sst.merlin.interface import *
+from sst.merlin.router import *
 
-_filename = os.path.basename(__file__)
+astra_dir    = pathlib.Path('/home/prlavin/sst/sst-astra-2/astra-sim') # TODO - set from ASTRA_ROOT
+examples_dir  = astra_dir / 'examples'
+workload_file = examples_dir / 'workload/microbenchmarks/all_gather/16npus_1MB/all_gather'
+system_file   = examples_dir / 'system/native_collectives/Ring_4chunks.json'
+memory_file   = examples_dir / 'remote_memory/analytical/no_memory_expansion.json'
+topo_file     = examples_dir / 'network/ns3/sample_16nodes_1D.json'
+
+FILE = os.path.basename(__file__)
 
 def checkFile(path):
     if not path.exists():
         raise FileNotFoundError(f"No such file: {path}")
 
+for f in [astra_dir, examples_dir, system_file, memory_file, topo_file]:
+    checkFile(f)
+
 def parseTopoFile(path):
-    print(f"{_filename}: parsing topo_file: {path}")
+    print(f"{FILE}: parsing topo_file: {path}")
     f = open(path, "r")
     try:
         data = json.load(f)
@@ -32,70 +46,43 @@ def parseTopoFile(path):
         numNPUs *= int(i)
 
     topo = '[' + topo + ']'
-    print(f"{_filename}: Result: {topo}")
+    print(f"{FILE}: Result: {topo}")
 
     return topo, numNPUs
 
-
-examples_dir  = astra_dir / 'examples'
-workload_file = examples_dir / 'workload/microbenchmarks/all_gather/16npus_1MB/all_gather'
-system_file   = examples_dir / 'system/native_collectives/Ring_4chunks.json'
-memory_file   = examples_dir / 'remote_memory/analytical/no_memory_expansion.json'
-topo_file     = examples_dir / 'network/ns3/sample_16nodes_1D.json'
-
-for f in [astra_dir, examples_dir, system_file, memory_file, topo_file]:
-    checkFile(f)
-
-topo, numNPUs = parseTopoFile(topo_file)
-
-# Create components
-workload = sst.Component('workload', 'astra.AstraWorkload')
-
-workload.addParams(
-    {
-        "workloadConfig":        workload_file,
-        "systemConfig":          system_file,
-        "memoryConfig":          memory_file,
-        "logicalTopologyConfig": topo,
-        "commGroupConfig":      "empty",
-    }
-)
-
-
-## Merlin Config
-merlinTopo = topoTorus()
-
-sst.merlin._params["torus.shape"] = "4x2x2"
-sst.merlin._params["torus.width"] = "1x1x1"
-sst.merlin._params["torus.local_ports"] = "1"
-sst.merlin._params["num_dims"] = "3"
-
-sst.merlin._params["link_bw"] = "100Gb/s"
-sst.merlin._params["link_lat"] = "20ns"
-sst.merlin._params["flit_size"] = "64b"
-sst.merlin._params["xbar_bw"] = "100Gb/s" # Should this be higher?
-
-sst.merlin._params["input_latency"] = "20ns"
-sst.merlin._params["output_latency"] = "20ns"
-sst.merlin._params["input_buf_size"] = "10kB"
-sst.merlin._params["output_buf_size"] = "10kB"
-
-sst.merlin._params["xbar_arb"] = "merlin.xbar_arb_lru"
-
-class AstraEndPoint(EndPoint):
-    def __init__(self, workload):
-        EndPoint.__init__(self)
-        self.workload = workload
+class AstraJob(Job):
+    def __init__(self, job_id, size, workload):
+        Job.__init__(self, job_id, size)
+        self._declareClassVariables(["_workload"])
+        self._workload = workload
     def getName(self):
-        return "AstraEndPoint"
-    def prepParams(self):
-        pass
+        return "AstraJob"
     def build(self, nID, extraKeys):
-        return (self.workload, f"port{nID}", sst.merlin._params['link_lat'])
+        print(f"building {nID}")
+        return (self._workload, f"port{nID}")
 
-endPoint = AstraEndPoint(workload)
+if __name__ == "__main__":
 
-merlinTopo.prepParams()
-# endPoint.prepParams()
-merlinTopo.setEndPoint(endPoint)
-merlinTopo.build()
+    # Create AstraSim component
+    topo, numNPUs = parseTopoFile(topo_file)
+
+    workload = sst.Component('workload', 'astra.AstraWorkload')
+    workload.addParams(
+        {
+            "workloadConfig":        workload_file,
+            "systemConfig":          system_file,
+            "memoryConfig":          memory_file,
+            "logicalTopologyConfig": topo,
+            "commGroupConfig":      "empty",
+        }
+    )
+
+    ep = AstraJob(0, 16, workload)
+
+    # Merlin settings
+    PlatformDefinition.loadPlatformFile("platform_file_dragon_16")
+    PlatformDefinition.setCurrentPlatform("platform_dragon_16")
+    system = System()
+
+    system.allocateNodes(ep, "linear")
+    system.build()
---
+++ src/sst/elements/astra/tests/platform_file_dragon_16.py
@@ -0,0 +1,53 @@
+import sst
+from sst.merlin.base import *
+
+platdef = PlatformDefinition("platform_dragon_16")
+PlatformDefinition.registerPlatformDefinition(platdef)
+
+
+platdef.addParamSet("topology",{
+    "hosts_per_router" : 4,
+    "routers_per_group" : 2,
+    "intergroup_links" : 4,
+    "num_groups" : 2,
+    "algorithm" : "adaptive-local",
+    "link_latency" : "20ns"
+})
+
+platdef.addClassType("topology","sst.merlin.topology.topoDragonFly")
+
+platdef.addParamSet("router",{
+    "link_bw" : "100Gb/s",
+    "flit_size" : "8B",
+    "xbar_bw" : "100Gb/s",
+    "input_latency" : "20ns",
+    "output_latency" : "20ns",
+    "input_buf_size" : "10kB",
+    "output_buf_size" : "10kB",
+    # Set up the number of VNs to use; using 2 since one of the jobs
+    # remaps to VN 1
+    "num_vns" : 1,
+    # Optionally set up the QOS
+    #"qos_settings" : [50,50]
+
+    # Set up the arbitration type for the routers
+    "xbar_arb" : "merlin.xbar_arb_lru",
+
+})
+
+platdef.addClassType("router","sst.merlin.base.hr_router")
+
+platdef.addParamSet("network_interface",{
+    "link_bw" : "100Gb/s",
+    "input_buf_size" : "10kB",
+    "output_buf_size" : "10kB"
+})
+
+#platdef.addClassType("network_interface","sst.merlin.base.ReorderLinkControl")
+platdef.addClassType("network_interface","sst.merlin.interface.ReorderLinkControl")
+
+
+platdef_cm = PlatformDefinition.compose("platform_dragon_16_cm",[("platform_dragon_16","ALL")])
+platdef_cm.addParamSet("router",{"enable_congestion_management":True})
+platdef_cm.addClassType("topology","sst.merlin.topology.topoDragonFly")
+platdef_cm.addClassType("network_interface","sst.merlin.interface.ReorderLinkControl")
