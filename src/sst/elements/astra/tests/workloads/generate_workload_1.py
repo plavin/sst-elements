a3d6ffdbd175b6eaa4391400e977b8e9aa43c37a
Patrick Lavin <plavin997@gmail.com>
2026-08-05 13:33:45 -0600

    workload generator script

---
+++ src/sst/elements/astra/tests/workloads/generate_workload_1.py
@@ -0,0 +1,3 @@
+# Using `main.py` from STAGE: https://github.com/astra-sim/stage/tree/main
+python main.py --output_dir workloads/ --output_name workload_1.%d.et --dp 2 --tp 1 --pp 2 --sp 1 --weight_sharded 0 --chakra_schema_version v0.0.4
+
